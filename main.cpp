220755c538e24a582398a17d182af98e3bfbd411
crismontecinos <cmontecinof@utem.cl>
2022-12-20 14:36:47 -0300

    segundo commit

--- main.cpp
+++ main.cpp
@@ -7,34 +7,87 @@
 #include <ctime>
 #include <cstring>
 #include <cmath>
+#include <omp.h>
 
 using namespace std;
 
-void calcularFecha(float numeroFecha){
+/**
+ * Funcion para convertir fechas a segundos gracias .
+ *
+ * @param anio tipo de puntero que apunta a una cadena de caracteres
+ * @param mes tipo de puntero que apunta a una cadena de caracteres
+ * @param dia tipo de puntero que apunta a una cadena de caracteres
+ * @return un valor de tipo time_t
+ */
+double fecha_a_segundos(char* anio, char* mes, char* dia) {
+  // Crear un objeto tm con la fecha dada
+  tm fecha;
+  fecha.tm_year = atoi(anio) - 1900;;
+  fecha.tm_mon = atoi(mes) - 1;
+  fecha.tm_mday = atoi(dia);
 
-    time_t seconds = numeroFecha;
+  // Convertir el objeto tm a un objeto time_t
+  time_t t = mktime(&fecha);
 
-    // Transformar los segundos en una estructura tm
-    struct tm *timeinfo = gmtime(&seconds);
-
-    // Imprimir la fecha y hora en el formato dd/mm/aaaa hh:mm:ss
-    cout << timeinfo->tm_mday << "/" << timeinfo->tm_mon + 1 << "/" << timeinfo->tm_year + 1900 << " ";
-    cout << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << endl;
+  // Obtener el número de segundos transcurridos desde la medianoche del 1 de enero de 1970
+  return t;
 }
 
-int main()
-{
-    ifstream miArchivo;
-    miArchivo.open("datos_examen.csv");
-    string linea = "";
-    float contador = 0;
+/**
+ * Esta función realiza una regresión lineal a partir de dos vectores de enteros, x e y, y
+ * un contador que indica el tamaño de ambos vectores. También recibe un flotante segundos
+ * para hacer la estimacion .
+ *
+ * @param x vector de enteros
+ * @param y vectores de enteros
+ * @param contador entero que indica el tamaño
+ * @param segundos es un flotante con los
+ * @return ecuaciondelaRecta
+ */
+
+float regresionLineal(vector<int> x,vector<int> y, int contador, float segundos ){
+
     float sumaAccidente = 0;
     float sumaYPromAccidente = 0;
     float sumaFecha = 0;
     float sumatoriaXY = 0;
     float sumatoriaXCuadrado = 0;
-    getline(miArchivo,linea);
-    while(getline(miArchivo,linea)){
+ #pragma omp parallel for reduction(+:sumaAccidente,sumatoriaXY,sumatoriaXCuadrado,sumaYPromAccidente,sumaFecha)
+    for(int i = 0; i<contador; i++){
+        sumaAccidente += y[i];
+        sumatoriaXY += (x[i] * y[i]);
+        sumatoriaXCuadrado += (y[i]*y[i]);
+        sumaYPromAccidente += (x[i]*x[i]);
+        sumaFecha +=x[i];
+    }
+    float promedioAccidente = sumaAccidente/contador;
+    float promeFecha = sumaFecha/contador;
+    float nxy = contador * promeFecha * promedioAccidente;
+    float nx2 = contador* promeFecha * promeFecha;
+    float pendiente = (sumatoriaXY - nxy)/(sumatoriaXCuadrado - nx2);
+    float ordenada = promedioAccidente - pendiente * promeFecha;
+    float ecuaciondelaRecta = ordenada + (pendiente * segundos);
+
+    return ecuaciondelaRecta;
+}
+
+int main()
+{
+    ifstream miArchivo;
+
+    miArchivo.open("datos_examen.csv");
+
+    string linea = "";
+
+    getline(miArchivo,linea);// salta la primera línea
+
+        vector<int> x;
+        vector<int> y;
+        int contador;
+        float segundos;
+
+//#pragma omp parallel
+   while(getline(miArchivo,linea)){
         string fecha;
         int accidentes;
 
@@ -43,66 +96,59 @@ int main()
         stringstream inputString(linea);
 
         getline(inputString, fecha, ';');
+
         //Dividimos la fecha en componentes utilizando strtok()
         char* date_str = strdup(fecha.c_str());
         char* day_str = strtok(date_str, "-");
         char* month_str = strtok(NULL, "-");
         char* year_str = strtok(NULL, "-");
-        //Convertimos los componentes a números y asignamos a la estructura tm
-        tm t;
-        t.tm_year = atoi(year_str) - 1900;
-        t.tm_mon = atoi(month_str) - 1;
-        t.tm_mday = atoi(day_str);
 
-        // Convertimos la estructura tm a un valor de tiempo en segundos
-        time_t segundos = mktime(&t);
-        sumaFecha = sumaFecha + segundos;
+        x.push_back(fecha_a_segundos(year_str , month_str, day_str));
+
         getline(inputString,tempString,';');
         accidentes = atoi(tempString.c_str());
+
+        y.push_back(accidentes);
+
         contador = contador + 1;
-        sumaAccidente = sumaAccidente + accidentes;
-        sumatoriaXY = sumatoriaXY +(segundos * accidentes);
-        sumatoriaXCuadrado = sumatoriaXCuadrado + (segundos*segundos);
-        sumaYPromAccidente = sumaYPromAccidente + (accidentes*accidentes);
         linea = "";
 
+}
+miArchivo.close();
+
+    string Dia,Mes,Anho;
+
+    string diaPrediccion;
+    while(true){
+    cout << "ingrese una fecha formato: " << endl;
+    cin >> diaPrediccion;
+    if (diaPrediccion.size() != 10 || diaPrediccion[4] != '/' || diaPrediccion[7] != '/' ){
+      cout << "Por favor, ingrese una fecha valida" << endl;
+      continue;
     }
-    float promedioAccidente = sumaAccidente/contador;
-    float promeFecha = sumaFecha/contador;
-    float nxy = contador * promeFecha * promedioAccidente;
-    float nx2 = contador* promeFecha * promeFecha;
-    float pendiente = (sumatoriaXY - nxy)/(sumatoriaXCuadrado - nx2);
-    float ordenada = promedioAccidente - pendiente * promeFecha;
-    float sumatoriaYCuadrado = sumaYPromAccidente;
-    double resultErEs = sqrt((sumatoriaYCuadrado- ordenada*sumaAccidente-pendiente*sumatoriaXY)/(contador-2));
-    cout <<"n: " <<contador << endl;
-    cout <<"Y promedio: " << promedioAccidente << endl;
-    cout << "X promedio: "  << promeFecha << endl;
-    cout << "Sumatoria X*Y: " << sumatoriaXY << endl;
-    cout << "n * X promedio * Y promedio: " << nxy << endl;
-    cout << "sumatoria X Cuadrado: " << sumatoriaXCuadrado << endl;
-    cout << "n* X promedio al cuadrado: " << nx2 << endl;
-    cout << "pendiente (b) " << pendiente << endl;
-    cout << "ordenada (a) " << ordenada << endl;
-    cout << "sumatoria Y cuadrada: " << sumatoriaYCuadrado << endl;
-    cout << "suma de y: " << sumaAccidente <<endl;
-    cout << "Error Estandar: " << resultErEs;
-
-    string diaSigiente = "2-09-2030";
-    char* date_str = strdup(diaSigiente.c_str());
-    char* day_str = strtok(date_str, "-");
+            // Verifica si los componentes de la fecha son números válidos
+    int _anhos, _meses, _dias;
+    if (!(istringstream(diaPrediccion.substr(0, 4)) >> _anhos)||
+        !(istringstream(diaPrediccion.substr(5, 2)) >> _meses)||
+        !(istringstream(diaPrediccion.substr(8, 2)) >> _dias)) {
+      cout << "Por favor, ingrese una fecha valida" << endl;
+      continue;
+    }
+     if (_meses < 1 || _meses > 12) {
+      cout << "Por favor, ingrese una fecha válida" << endl;
+      continue;
+        }
+    char* date_str = strdup(diaPrediccion.c_str());
+    char* year_str = strtok(date_str, "-");
     char* month_str = strtok(NULL, "-");
-    char* year_str = strtok(NULL, "-");
-        //Convertimos los componentes a números y asignamos a la estructura tm
-    tm t;
-    t.tm_year = atoi(year_str) - 1900;
-    t.tm_mon = atoi(month_str) - 1;
-    t.tm_mday = atoi(day_str);
+    char* day_str = strtok(NULL, "-");
 
-        // Convertimos la estructura tm a un valor de tiempo en segundos
-    time_t seg = mktime(&t);
-    float ecuaciondelaRecta = ordenada - (pendiente * seg);
-    cout << seg << endl;
-    cout << "Prediccion: " << ecuaciondelaRecta << endl;
+    segundos = fecha_a_segundos(year_str , month_str, day_str);
+    break;
+    }
+
+    float ecuaciondelarecta =regresionLineal(x, y, contador, segundos);
+
+    cout << "Prediccion: " << ecuaciondelarecta << " Fecha de la prediccion: " <<diaPrediccion << endl;
     return 0;
 }
